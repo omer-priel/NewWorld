170cdc89fe878ce29e56c744db54c8ac1a2ad2a4
Omer Priel <omer2079@gmail.com>
2021-08-19 17:29:51 +0300

    create NewWorldWindowsPlugin

---
+++ DevOps/IDEPlugin/NewWorldWindowsPlugin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWorldWindowsPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("args.Length: {0}", args.Length);
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
