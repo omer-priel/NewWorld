8d017bb172a67439faf884e5d51877c4627dda5f
Omer Priel <omer2079@gmail.com>
2021-08-21 22:40:59 +0300

    wip

---
+++ DevOps/IDEPlugin/NewWorldVisualStudioPlugin/NewWorldPackage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace NewWorldVisualStudioPlugin
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(NewWorldPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class NewWorldPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "7b70571c-601d-442d-8ac6-650bba89d678";
+
+        public NewWorldPackage()
+        {
+
+        }
+
+        #region Package Members
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+
+        #endregion
+    }
+}
