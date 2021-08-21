776f8358685dc05c0cc63b40a9d9235413e95d03
Omer Priel <omer2079@gmail.com>
2021-08-21 23:21:06 +0300

    fix command ids

--- DevOps/IDEPlugin/NewWorldVisualStudioPlugin/src/Commands/OpenWithVSCode.cs
+++ DevOps/IDEPlugin/NewWorldVisualStudioPlugin/src/Commands/OpenWithVSCode.cs
@@ -17,7 +17,7 @@ namespace NewWorldVisualStudioPlugin.Commands
         /// <summary>
         /// Command ID.
         /// </summary>
-        public const int CommandId = 4130;
+        public const int CommandId = 0x0102;
 
         /// <summary>
         /// Command menu group (command set GUID).
