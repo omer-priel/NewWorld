19f0bfd627d3d2720cb04eefadbd18e53ce552ef
Omer Priel <omer2079@gmail.com>
2021-08-24 12:15:54 +0300

    fix Build.py

--- DevOps/Scripts/src/Build.py
+++ DevOps/Scripts/src/Build.py
@@ -5,19 +5,9 @@ import Utilities.MSBuild as MSBuild
 
 Utilities.SetTitle("Build")
 
-jsonPath = Utilities.GetSubPath('DevOps\\IDEPlugin\\NewWorldVSCodePlugin\\package.json')
-newWorldSettings = Utilities.LoadJsonFile(Utilities.GetSubPath('NewWorld.nwe'))
-
-# build New World Engine
-projectFilePath = Utilities.GetSubPath('NewWorld\\NewWorld.vcxproj')
+# Build the Solution
+projectFilePath = Utilities.GetSubPath('NewWorld.sln')
 MSBuild.Clean(projectFilePath, 'Release')
 MSBuild.Build(projectFilePath, 'Release')
-
-# build projects
-for project in newWorldSettings.projects:
-    if ((build in project) and project.build):
-        projectFilePath = Utilities.GetSubPath(project.folder) + f'\\{project.name}.vcxproj';
-        MSBuild.Clean(projectFilePath, 'Release')
-        MSBuild.Build(projectFilePath, 'Release')
 
 Utilities.PresToConinue()
