fc248a5051929d3517635ca06031dac8e1f8f91e
Omer Priel <omer2079@gmail.com>
2021-09-01 12:59:30 +0300

    complit the temp Logger

--- NewWorld/src/NewWorld/Debug/Logger.h
+++ NewWorld/src/NewWorld/Debug/Logger.h
@@ -48,21 +48,76 @@ namespace NewWorld::Debug
 		Log("\n");
 	}
 
-	// Log Variadic types
-	template <typename T, typename... Types>
-	void Log(T var1, Types... args)
+	// Log Types
+	void Log() { }
+
+	template <typename... Types>
+	void Log(char value, Types... args)
 	{
+		std::cout << value;
 		Log(args...);
 	}
 
-	void Log() { }
+	template <typename... Types>
+	void Log(Byte value, Types... args)
+	{
+		std::cout << value;
+		Log(args...);
+	}
 
-	// Log Types
+	template <typename... Types>
+	void Log(short value, Types... args)
+	{
+		std::cout << value;
+		Log(args...);
+	}
+
+	template <typename... Types>
+	void Log(ushort value, Types... args)
+	{
+		std::cout << value;
+		Log(args...);
+	}
+
+	template <typename... Types>
+	void Log(int value, Types... args)
+	{
+		std::cout << value;
+		Log(args...);
+	}
+
+	template <typename... Types>
+	void Log(uint value, Types... args)
+	{
+		std::cout << value;
+		Log(args...);
+	}
+
+	template <typename... Types>
+	void Log(Long value, Types... args)
+	{
+		std::cout << value;
+		Log(args...);
+	}
+
+	template <typename... Types>
+	void Log(Ulong value, Types... args)
+	{
+		std::cout << value;
+		Log(args...);
+	}
+
+	template <typename... Types>
+	void Log(float value, Types... args)
+	{
+		std::cout << value;
+		Log(args...);
+	}
+
 	template <typename... Types>
 	void Log(const char* str, Types... args)
 	{
 		std::cout << str;
-
 		Log(args...);
 	}
 
@@ -70,7 +125,6 @@ namespace NewWorld::Debug
 	void Log(const String& str, Types... args)
 	{
 		std::cout.write(str.GetPointer(), str.GetLength());
-
 		Log(args...);
 	}
 
@@ -80,9 +134,17 @@ namespace NewWorld::Debug
 		Log(obj.ToString(), args...);
 	}
 
-	template <typename... Types>
-	void Log(const Object& obj, Types... args)
+	template <typename T, typename... Types>
+	void Log(const T& obj, Types... args)
 	{
-		Log((const IObject&)obj, args...);
+		if (std::is_base_of<IObject, T>::value)
+		{
+			Log((const IObject&)obj);
+		}
+		else
+		{
+			throw "ERROR";
+		}
 	}
+
 }
