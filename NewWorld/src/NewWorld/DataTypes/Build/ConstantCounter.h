fc630fa05b3a17ae890236baa4d10b5905ac8e16
Omer Priel <omer2079@gmail.com>
2021-10-26 00:55:32 +0300

    wip

--- NewWorld/src/NewWorld/DataTypes/Build/ConstantCounter.h
+++ NewWorld/src/NewWorld/DataTypes/Build/ConstantCounter.h
@@ -1,9 +1,7 @@
 #pragma once
 
-template<int N>
-struct flag {
-    friend constexpr int adl_flag(flag<N>);
-};
+template<int N> class flag { };
+
 template<int N>
 struct writer {
     friend constexpr int adl_flag(flag<N>) {
@@ -13,32 +11,23 @@ struct writer {
     static constexpr int value = N;
 };
 
-template<int N, int = adl_flag(flag<N> {}) >
+template<int N, int = adl_flag(flag<N>{})>
 int constexpr reader(int, flag<N>) {
     return N;
 }
 
 template<int N>
-int constexpr reader(float, flag<N>, int R = reader(0, flag<N - 1> {})) {
+int constexpr reader(float, flag<N>, int R = reader(0, flag<N - 1>{})) {
     return R;
 }
 
 int constexpr reader(float, flag<0>) {
     return 0;
 }
-template<int N = 1>
-int constexpr next(int R = writer < reader(0, flag<32> {}) + N > ::value) {
-    return R;
-}
 
-template<int N = 1, int C = reader(0, flag<32>())>
-int constexpr next(int R = writer<C + N>::value) {
-    return R;
-}
-
-template<typename T, int N = 1>
+template<typename T, int N = 0>
 struct Generator {
-    static constexpr int next = writer < reader(0, flag<32> {}) + N > ::value; // 32 implies maximum UUID of 32
+    static constexpr int ID = writer<reader(N, flag<32>{}) + 1>::value; // 32 implies maximum UUID of 32
 };
 
 /*
--- Sandbox/src/Tests/Main.h
+++ Sandbox/src/Tests/Main.h
@@ -14,13 +14,17 @@ namespace Sandbox::Tests
 			SharedPointer<String> ptr("Test1");
 			ScopePointer<String> ptr2("Test2");
 
-			constexpr int a = Generator<IObject>::next;
-			constexpr int b = Generator<Object>::next;
-			constexpr int c = Generator<String>::next;
+			constexpr int a = Generator<IObject>::ID;
+			constexpr int b = Generator<Object>::ID;
+			constexpr int c = Generator<String>::ID;
+			constexpr int d = Generator<Object>::ID;
+			constexpr int e = Generator<IObject>::ID;
 
 			NW_DEBUG("IObject: ", a);
 			NW_DEBUG("Object: ", b);
 			NW_DEBUG("String: ", c);
+			NW_DEBUG("d: ", d);
+			NW_DEBUG("e: ", e);
 
 			NW_DEBUG(ptr, " ", ptr2, "\n", ptr.GetType());
 		}
