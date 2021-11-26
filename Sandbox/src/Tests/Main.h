243eb03a7b6397b6927e5fa69b2f082825df46cc
Omer Priel <omer2079@gmail.com>
2021-11-26 14:13:36 +0200

    wip

--- Sandbox/src/Tests/Main.h
+++ Sandbox/src/Tests/Main.h
@@ -14,17 +14,15 @@ namespace Sandbox::Tests
 			SharedPointer<String> ptr("Test1");
 			ScopePointer<String> ptr2("Test2");
 
-			constexpr int a = Generator<IObject>::ID;
-			constexpr int b = Generator<Object>::ID;
-			constexpr int c = Generator<String>::ID;
-			constexpr int d = Generator<Object>::ID;
-			constexpr int e = Generator<IObject>::ID;
-
-			NW_DEBUG("IObject: ", a);
-			NW_DEBUG("Object: ", b);
-			NW_DEBUG("String: ", c);
-			NW_DEBUG("d: ", d);
-			NW_DEBUG("e: ", e);
+			NW_DEBUG("a: ", Generator<IObject>::ID);
+			NW_DEBUG("b: ", Generator<Object>::ID);
+			NW_DEBUG("c: ", Generator<String>::ID);
+			NW_DEBUG("d: ", Generator<Example>::ID);
+			NW_DEBUG("e: ", Generator<SharedPointer<String>>::ID);
+			NW_DEBUG("f: ", Generator<ScopePointer<String>>::ID);
+			NW_DEBUG("g: ", Generator<IObject>::ID);
+			NW_DEBUG("h: ", Generator<Object>::ID);
+			NW_DEBUG("i: ", Generator<String>::ID);
 
 			NW_DEBUG(ptr, " ", ptr2, "\n", ptr.GetType());
 		}
