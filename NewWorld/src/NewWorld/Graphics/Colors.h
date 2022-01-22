894b2067de3403b69c428c764680561897307670
Omer Priel <omer2079@gmail.com>
2022-01-22 20:17:55 +0200

    add new colors

--- NewWorld/src/NewWorld/Graphics/Colors.h
+++ NewWorld/src/NewWorld/Graphics/Colors.h
@@ -20,9 +20,14 @@ namespace NewWorld::Graphics
 		static constexpr Color Blue = Color(0, 0, 1);
 
 		// Simple Colors
-		static constexpr Color Yellow = Color(1, 1, 0);
-		static constexpr Color Magenta = Color(1, 0, 1);
 		static constexpr Color Cyan = Color(0, 1, 1);
+		static constexpr Color Magenta = Color(1, 0, 1);
+		static constexpr Color Yellow = Color(1, 1, 0);
+		static constexpr Color Gray = Color(0.5f, 0.5f, 0.5f);
+		static constexpr Color DarkGray = Color(0.25f, 0.25f, 0.25f);
+		static constexpr Color LightGray = Color(0.75f, 0.75f, 0.75f);
+		static constexpr Color Orange = Color(1.0f, 0.5f, 0.0f);
+		static constexpr Color Pink = Color(1.0f, 0.625f, 0.625f);
 
 		// Editor Colors
 		static constexpr Color EditorBackground = Color(0.2f, 0.2f, 0.2f);
