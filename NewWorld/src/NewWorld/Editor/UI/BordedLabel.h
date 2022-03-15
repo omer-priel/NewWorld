73945acfb5c82f468eae5d56583a13934c282a02
Omer Priel <omer2079@gmail.com>
2022-03-16 00:20:34 +0200

    create BordedLabel

---
+++ NewWorld/src/NewWorld/Editor/UI/BordedLabel.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "NewWorld/Minimal.h"
+#include "NewWorld/Editor/UI/Label.h"
+
+namespace NewWorld::Editor::UI
+{
+	class BordedLabel : public Label
+	{
+	NW_CLASS(NewWorld::Editor::UI, BordedLabel)
+
+		// Members
+	private:
+		Graphics::Color m_BordedColor;
+
+	public:
+		BordedLabel(uint x, uint y, const String& text, uint width = 0,
+			const Graphics::Color& backgroundColor = Graphics::Colors::Transparency,
+			const Graphics::Color& textColor = Graphics::Colors::White,
+			const Graphics::Color& bordedColor = Graphics::Colors::Black,
+			uint fontSize = LABEL_DEFUALT_FONT_SIZE)
+			: Label(x, y, text, width, backgroundColor, textColor, fontSize), m_BordedColor(bordedColor)
+		{
+
+		}
+
+		// Events
+	public:
+		void Update() override;
+
+		// Getters
+	public:
+		inline const Graphics::Color& GetBordedColor() const { return m_BordedColor; }
+
+		// Setters
+	public:
+		void SetBordedColor(const Graphics::Color& bordedColor) { m_BordedColor = bordedColor; }
+	};
+}
