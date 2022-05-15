cae68f1ae42d9ec87e5f5f90a8edbb95ae42be0b
Omer Priel <omer2079@gmail.com>
2022-05-15 19:58:24 +0300

    wip

---
+++ NewWorld/src/NewWorld/Editor/Components/IComponent.cpp
@@ -0,0 +1,9 @@
+#include "nwpch.h"
+#include "IComponent.h"
+
+#include "NewWorld/Editor/EditorWindow.h"
+
+namespace NewWorld::Editor::Components
+{
+	
+}
---
+++ NewWorld/src/NewWorld/Editor/Components/IComponent.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "NewWorld/Minimal.h"
+#include "NewWorld/Input/Key.h"
+
+namespace NewWorld::Editor
+{
+	class EditorWindow;
+}
+
+namespace NewWorld::Editor::Components
+{
+	class IComponent : virtual public IObject
+	{
+		// Getters
+	public:
+		virtual inline RawPointer<EditorWindow> GetWindow() const = 0;
+
+		virtual inline float GetX() const = 0;
+		virtual inline float GetY() const = 0;
+
+		virtual inline bool IsIn(float x, float y) const = 0;
+
+		//virtual inline float GetWidth() const { return m_Width; }
+		//virtual inline float GetHeight() const { return m_Height; }
+
+		// Setters
+	public:
+		virtual void SetWindow(RawPointer<EditorWindow> window) = 0;
+
+		// Events
+	public:
+		virtual void Update() {}
+
+		virtual void Create() = 0;
+		virtual void Destroy() = 0;
+
+		virtual void Enter() = 0;
+		virtual void Leave() = 0;
+
+		virtual void Click() = 0;
+
+		virtual void MouseKeyPressed(Input::Key key, float xPos, float yPos) = 0;
+		virtual void MouseKeyReleased(Input::Key key, float xPos, float yPos) = 0;
+
+		virtual void MouseHover(float xPos, float yPos) = 0;
+		virtual void MouseScrolled(uint y) = 0;
+
+		virtual void KeyPressed(Input::Key key) = 0;
+		virtual void KeyReleased(Input::Key key) = 0;
+	};
+}
